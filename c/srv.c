c60b1b6e423a5222d1464050e6f5ea30f0844b33
Timon van der Berg <tmnvanderberg@gmail.com>
2023-01-06 16:14:50 +0100

    create socket

--- c/srv.c
+++ c/srv.c
@@ -1,5 +1,18 @@
+#include <errno.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main() {
-				printf("Hello, World!\n");
+  const int CHOOSE_PROTOCOL_AUTOMATICALLY = 0;
+	const int PORT = 8080;
+  int socket_fd = socket(AF_INET, SOCK_STREAM, CHOOSE_PROTOCOL_AUTOMATICALLY);
+  if (socket_fd == -1) {
+    perror("webserver (socket)");
+    return 1;
+  }
+  printf("socket created succesfully\n");
+	/* bind(socket_fd, sockaddr *address, */
+           /* address_len); */
+  return 0;
 }
